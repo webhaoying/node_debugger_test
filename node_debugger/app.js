56525777eb36169f8297cbfa146d469477edbb12
zhanghao <zlyheut@163.com>
2019-07-02 18:51:59 +0800

    node 调试

--- node_debugger/app.js
+++ node_debugger/app.js
@@ -4,10 +4,10 @@ const app = new Koa();
 const main = ctx => {
   ctx.response.body = ' Hello World ';
 };
-const welcome = ctx => {
-  ctx.response.body = ' Hello World ';
+const welcome = (ctx, name) => {
+  ctx.response.body = ` Hello ${name} `;
 };
 app.use(router.get('/', main));
-app.use(router.get('/:name',welcome));
+app.use(router.get('/:name', welcome));
 app.listen(3001);
 console.log('listening on port 3001');
